6f443e5b6758ad5bbb367f51987ca51176c7697b
ProfBat018 <61077831+ProfBat018@users.noreply.github.com>
2021-06-22 11:44:12 +0400

    Add project files.

---
+++ FBES_1211_RU/Source.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	/*
+	float S = 377;
+	float V = 90;
+	float t = 0;
+
+	t = S / V;
+	
+	cout << "Time is: " << t << " " << "hours";
+	*/
+
+	// Data lost 
+	int num = 3; // Любое целое число в диапозоне от -2 милл. до +2 милл.
+
+
+	num = 3.25; // не принимает дробное число, а оставляет только целую часть.
+	cout << num << endl;
+
+
+
+	return 0;
+}
