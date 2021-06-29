3b4d27a3f87f42f6973c318f48de24287459f90e
ProfBat018 <61077831+ProfBat018@users.noreply.github.com>
2021-06-29 11:51:06 +0400

    Last version

--- comparison statements/Source.cpp
+++ comparison statements/Source.cpp
@@ -2,10 +2,152 @@
 
 using namespace std;
 
+// ctrl + k + d beautify your code.
+
 int main()
 {
-	cout << "Hello World" << endl;
+	/*
+	// else if - иначе если
+	// else - иначе
+	// { - begin
+	// } - end
+	// можно писать без {} если внутри условия всего одна строка.
 
+	if (true) // будет работать, потому что всегда true при работе программы.
+	{
+		cout << "Hello World" << endl;
+	}
+
+	if (false) // будет работать, потому что всегда true при работе программы.
+		cout << "Hello World" << endl;
+	*/
+
+	/*
+	int num = 2;
+
+	if (num == 2)
+	{
+		cout << 2; // 2
+	}
+	else if (num == 3)
+	{
+		cout << 3; // no
+	}
+	else if (num == 4)
+	{
+		cout << 4; // no
+	}
+	else
+	{
+		cout << 1; // no
+	}
+
+
+	if (num == 2)
+	{
+		cout << 2; // 2
+	}
+	if (num == 3)
+	{
+		cout << 3; // no
+	}
+	if (num == 4)
+	{
+		cout << 4; // no
+	}
+	else
+	{
+		cout << 1; // 1
+	}
+
+	*/
+
+	/*
+	// ? - if
+	// : - else
+	int test = 3;
+
+	cout << (test % 2 == 0 ? "Yes" : (test == 1 ? "Hello" : (test == 3 ? "3" : "-3"))) << endl;
+
+
+	int test2 = 3;
+
+	if (test % 2 == 0)
+	{
+		cout << "Yes";
+	}
+	else
+	{
+		if (test == 1)
+		{
+			cout << "Hello";
+		}
+		else
+		{
+			if (test == 3)
+			{
+				cout << "3";
+			}
+			else
+			{
+				cout << "-3";
+			}
+		}
+	}
+
+
+	if (test % 2 == 0)
+	{
+		cout << "Yes";
+	}
+	else if (test == 1)
+	{
+		cout << "Hello";
+	}
+	else if (test == 3)
+	{
+		cout << "3";
+	}
+	else
+	{
+		cout << "-3";
+	}
+
+	*/
+
+	/*
+	
+	int first_number = 0, second_number = 0;
+	int result = 0;
+
+	cout << "Enter your first number: ";
+	cin >> first_number;
+
+	cout << "Enter your second number: ";
+	cin >> second_number;
+
+	result = first_number + second_number;
+
+	if (result > 0 && result % 2 == 0) // && - and, || - or
+	{
+		cout << "Positive and even" << endl;
+	}
+	else if (result > 0 && result % 2 != 0)
+	{
+		cout << "Positive and odd" << endl;
+	}
+	else if (result == 0)
+	{
+		cout << "Equal to zero" << endl;
+	}
+	else
+	{
+		cout << "Negative" << endl;
+	}
+	*/
+	
+
+	
 
 	return 0;
 }
