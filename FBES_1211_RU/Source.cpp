b0fa416c8e39509ca9960b64fd591e8fa7408ac2
ProfBat018 <61077831+ProfBat018@users.noreply.github.com>
2021-06-29 09:09:10 +0400

    second test project

--- FBES_1211_RU/Source.cpp
+++ FBES_1211_RU/Source.cpp
@@ -5,24 +5,10 @@ using namespace std;
 
 int main()
 {
-	/*
-	float S = 377;
-	float V = 90;
-	float t = 0;
+	setlocale(LC_ALL, "russian");
 
-	t = S / V;
-	
-	cout << "Time is: " << t << " " << "hours";
-	*/
-
-	// Data lost 
-	int num = 3; // Любое целое число в диапозоне от -2 милл. до +2 милл.
-
-
-	num = 3.25; // не принимает дробное число, а оставляет только целую часть.
-	cout << num << endl;
-
-	cout << "Update";
+	cout << (char)205 << (char)205 << (char)205 << (char)205;
 
 	return 0;
 }
+
